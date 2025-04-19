67024dec1f53d87a10fc4d0841cee9fe4058a85f
Juswa <jgonobsit@tfvc.edu.ph>
2025-04-19 11:58:28 +0800

    habol

--- client/src/pages/admin/AdminStocksPending.jsx
+++ client/src/pages/admin/AdminStocksPending.jsx
@@ -3,11 +3,11 @@ import AdminHeader from "../../reusable/Admin/AdminHeader";
 import axiosInstance from "../../lib/axios";
 import { useState } from "react";
 import AdminOrderStocksModal from "./AdminOrderStocksModal";
+import LoadingSpinner from "../../reusable/LoadingSpinner";
 
 export default function AdminStocksPending() {
   const [orderSingleStock, setOrderSingleStock] = useState();
   const [openModal, setOpenModal] = useState(false);
-
   const [selectedId, setSelectedId] = useState(null);
 
   const {
@@ -64,46 +64,54 @@ export default function AdminStocksPending() {
           <div className="absolute bg-card -top-7 right-0 w-[80px] border border-black h-[20px] rounded-full"></div>
 
           <div className="p-4 flex flex-col gap-4">
-            {products.length > 0 ? (
-              products.map((product) => (
-                <div key={product._id} className="flex items-center gap-4">
-                  <img
-                    src={product?.productImages[0]}
-                    alt=""
-                    className="h-[50px] w-[50px] object-cover rounded-[5px]"
-                  />
-                  <div className="flex justify-between w-full items-center">
-                    <div className="flex gap-4">
-                      <div className="flex gap-2 items-center">
-                        <p className="text-blue-700">Product Name: </p>
-                        <p>{product?.productName}</p>
-                      </div>
-                      <div className="hidden md:flex md:gap-4">
-                        <div className="flex gap-2 items-center">
-                          <p className="text-blue-700">Description: </p>
-                          <p>{product?.productDescription}</p>
-                        </div>
-
-                        <div className="flex gap-2 items-center">
-                          <p className="text-blue-700">Category: </p>
-                          <p>{product?.category?.categoryName}</p>
-                        </div>
-                      </div>
-                    </div>
-                    <div>
-                      <button
-                        onClick={() => handleOpenSingleOrder(product)}
-                        className="border bg-primary rounded-[5px] text-white p-1 border-black"
-                      >
-                        Order Stock
-                      </button>
-                    </div>
-                  </div>
+            {
+              isProductsPending ? (
+                <div className="flex justify-center">
+                  <LoadingSpinner/>
                 </div>
-              ))
-            ) : (
-              <p>No Pendings</p>
-            )}
+              ) : (
+                products.length > 0 ? (
+                  products.map((product) => (
+                    <div key={product._id} className="flex items-center gap-4">
+                      <img
+                        src={product?.productImages[0]}
+                        alt=""
+                        className="h-[50px] w-[50px] object-cover rounded-[5px]"
+                      />
+                      <div className="flex justify-between w-full items-center">
+                        <div className="flex gap-4">
+                          <div className="flex gap-2 items-center">
+                            <p className="text-blue-700">Product Name: </p>
+                            <p>{product?.productName}</p>
+                          </div>
+                          <div className="hidden md:flex md:gap-4">
+                            <div className="flex gap-2 items-center">
+                              <p className="text-blue-700">Description: </p>
+                              <p>{product?.productDescription}</p>
+                            </div>
+    
+                            <div className="flex gap-2 items-center">
+                              <p className="text-blue-700">Category: </p>
+                              <p>{product?.category?.categoryName}</p>
+                            </div>
+                          </div>
+                        </div>
+                        <div>
+                          <button
+                            onClick={() => handleOpenSingleOrder(product)}
+                            className="border bg-primary rounded-[5px] text-white p-1 border-black"
+                          >
+                            Order Stock
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  ))
+                ) : (
+                  <p>No Pendings</p>
+                )
+              )
+            }
           </div>
         </div>
       </div>
