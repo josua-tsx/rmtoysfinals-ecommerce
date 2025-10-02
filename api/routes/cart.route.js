2576036edc118e3e7b6572ff6a56ef63b9779a65
Juswa <jgonobsit@tfvc.edu.ph>
2025-10-02 21:36:13 +0800

    habol

--- api/routes/cart.route.js
+++ api/routes/cart.route.js
@@ -5,6 +5,7 @@ import {
   addToCart,
   deleteCart,
   getCarts,
+  getSelectedCart,
   updateQuantity,
   updateSelect,
 } from "../controllers/cart.controller.js";
@@ -13,6 +14,7 @@ const router = express.Router();
 
 router.post(`/`, requireAuth, addToCart);
 router.get(`/get`, requireAuth, getCarts);
+router.get(`/get-selecteds`, requireAuth, getSelectedCart);
 router.delete(`/delete`, requireAuth, deleteCart);
 router.put(`/update-select/:productId`, requireAuth, updateSelect);
 router.post(`/updateQuantity`, requireAuth, updateQuantity);
