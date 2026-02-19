8d004496290c71b3418d450e32ef64385a9d5f88
juswa <jgonobsit@tfvc.edu.ph>
2026-02-19 12:29:00 +0800

    updated

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -2,21 +2,20 @@ import { useQuery } from "@tanstack/react-query";
 import { FaShoppingCart } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import axiosInstance from "../lib/axios";
-import { useEffect, useState } from "react";
 
 export default function Cart() {
   const {
     data: cart = [],
     isLoading,
     isError,
-} = useQuery({
+  } = useQuery({
     queryKey: ["cart"],
     queryFn: async () => {
-      const res = await axiosInstance.get(`/cart/get`);
-      return res.data  // Ensure we always return an object with items
+      const res = await axiosInstance.get(`/cart/get?limit=100000`);
+      return res.data; // Ensure we always return an object with items
     },
   });
-  
+
   if (isLoading) return <p>loading..</p>;
   if (isError) return <p>loading..</p>;
 
@@ -25,18 +24,14 @@ export default function Cart() {
       <Link to={`/cart`} className="relative">
         <FaShoppingCart size={25} />
 
-        {
-          cart?.items?.length > 0 && (
-            <span
+        {cart?.items?.length > 0 && (
+          <span
             className={`absolute bg-red-600 p-2 h-[20px] flex w-[20px] text-sm text-white 
            items-center justify-center -bottom-2 -right-[9.5px] rounded-full`}
           >
             {cart?.items?.length}
           </span>
-    
-          )
-        }
-         
+        )}
       </Link>
     </div>
   );
