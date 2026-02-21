1422c3ff3517edfdab4d8ae640ae6bebf9e259a1
juswa <jgonobsit@tfvc.edu.ph>
2026-02-21 19:12:46 +0800

    up up

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -49,7 +49,6 @@ export const signup = async (req, res, next) => {
       username,
       password,
       role: admin ? "customer" : "admin",
-      isPhoneVerified: true,
     });
 
     // authenticate
